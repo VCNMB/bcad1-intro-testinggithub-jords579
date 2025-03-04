ada94cdde65b5a5cb8193e679c9992e60f0b90b5
jords579 <154249382+jords579@users.noreply.github.com>
2025-03-04 11:27:46 +0200

    added prjClass
    Adding prjClass from neatbeans

---
+++ prjClasses/src/main/java/vcconnect/edu/za/prjclasses/PrjClasses.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vcconnect.edu.za.prjclasses;
+
+/**
+ *
+ * @author jorda
+ */
+public class PrjClasses {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
